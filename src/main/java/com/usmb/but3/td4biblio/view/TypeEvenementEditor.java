2707ec1845d824d0f53f911d0473033c0edf5f11
AlexisBONNARD <alexis.bonnard@etu.univ-smb.fr>
2026-06-12 09:53:59 +0200

    Add: Evenement_View_et_Editor

---
+++ src/main/java/com/usmb/but3/td4biblio/view/TypeEvenementEditor.java
@@ -0,0 +1,96 @@
+package com.usmb.but3.td4biblio.view;
+
+import org.springframework.context.annotation.Scope;
+
+import com.usmb.but3.td4biblio.entity.TypeEvenement;
+import com.usmb.but3.td4biblio.service.TypeEvenementService;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+
+@Scope("prototype")
+@SpringComponent
+@UIScope
+public class TypeEvenementEditor extends VerticalLayout implements KeyNotifier {
+
+    private final TypeEvenementService typeEvenementService;
+    private TypeEvenement typeEvenement;
+
+    TextField nom = new TextField("Nom du type d'événement");
+
+    HorizontalLayout fields = new HorizontalLayout(nom);
+
+    Button save = new Button("Sauvegarder", VaadinIcon.CHECK.create());
+    Button cancel = new Button("Annuler");
+    Button delete = new Button("Supprimer", VaadinIcon.TRASH.create());
+    HorizontalLayout actions = new HorizontalLayout(save, cancel, delete);
+
+    Binder<TypeEvenement> binder = new Binder<>(TypeEvenement.class);
+    private ChangeHandler changeHandler;
+
+    public TypeEvenementEditor(TypeEvenementService service) {
+        this.typeEvenementService = service;
+
+        add(fields, actions);
+
+        binder.bindInstanceFields(this);
+
+        setSpacing(true);
+
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+
+        addKeyPressListener(Key.ENTER, e -> save());
+
+        save.addClickListener(e -> save());
+        delete.addClickListener(e -> delete());
+        cancel.addClickListener(e -> editTypeEvenement(typeEvenement));
+        setVisible(false);
+    }
+
+    void delete() {
+        typeEvenementService.deleteTypeEvenementById(typeEvenement.getId());
+        changeHandler.onChange();
+    }
+
+    void save() {
+        typeEvenementService.saveTypeEvenement(typeEvenement);
+        changeHandler.onChange();
+    }
+
+    public interface ChangeHandler {
+        void onChange();
+    }
+
+    public final void editTypeEvenement(TypeEvenement te) {
+        if (te == null) {
+            setVisible(false);
+            return;
+        }
+        final boolean persisted = te.getId() != null;
+        if (persisted) {
+            typeEvenement = typeEvenementService.getTypeEvenementById(te.getId());
+        } else {
+            typeEvenement = te;
+        }
+        cancel.setVisible(persisted);
+
+        binder.setBean(typeEvenement);
+
+        setVisible(true);
+
+        nom.focus();
+    }
+
+    public void setChangeHandler(ChangeHandler h) {
+        changeHandler = h;
+    }
+}
---
+++ src/main/java/com/usmb/but3/td4biblio/view/TypeEvenementView.java
@@ -0,0 +1,75 @@
+package com.usmb.but3.td4biblio.view;
+
+import com.usmb.but3.td4biblio.entity.TypeEvenement;
+import com.usmb.but3.td4biblio.service.TypeEvenementService;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+@Scope("prototype")
+@Route("type-evenement")
+@PageTitle("Types d'événements")
+@Menu(title = "Types d'événements", order = 3, icon = "vaadin:calendar")
+public class TypeEvenementView extends VerticalLayout {
+    private final TypeEvenementService typeEvenementService;
+    final Grid<TypeEvenement> grid;
+    final TextField filter;
+    final Button addNewBtn;
+    final TypeEvenementEditor editor;
+
+    public Button getAddNewBtn() {
+        return addNewBtn;
+    }
+
+    public TypeEvenementView(TypeEvenementService typeEvenementService, TypeEvenementEditor editor) {
+        this.typeEvenementService = typeEvenementService;
+        this.editor = editor;
+        this.grid = new Grid<>(TypeEvenement.class);
+        this.filter = new TextField();
+        this.addNewBtn =
+                new Button("Ajouter un type", VaadinIcon.PLUS.create());
+
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+        add(actions, grid, editor);
+
+        grid.setHeight("300px");
+        grid.setColumns("id", "nom");
+        grid.getColumnByKey("id").setWidth("60px").setFlexGrow(0);
+
+        filter.setPlaceholder("Filtrer par nom");
+
+        filter.setValueChangeMode(ValueChangeMode.LAZY);
+        filter.addValueChangeListener(e -> listTypeEvenements(e.getValue()));
+
+        grid.asSingleSelect().addValueChangeListener(e -> editor.editTypeEvenement(e.getValue()));
+
+        addNewBtn.addClickListener(e -> editor.editTypeEvenement(new TypeEvenement()));
+
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            listTypeEvenements(filter.getValue());
+        });
+
+        listTypeEvenements(null);
+    }
+
+    void listTypeEvenements(String filterText) {
+        if (StringUtils.hasText(filterText)) {
+            grid.setItems(typeEvenementService.getTypeEvenementsByNomLike("%" + filterText + "%"));
+        } else {
+            grid.setItems(typeEvenementService.getAllTypeEvenements());
+        }
+    }
+}
