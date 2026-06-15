7c4b6249cd4417741cf92c30fe7165d36999c710
AR9-LegAcy <ar9legacy.main@gmail.com>
2026-06-15 10:55:42 +0200

    fix: modification_de_l_entite_livre_ainsi_que_des_fichiers_qui_le_suive

--- src/main/java/com/usmb/but3/td4biblio/entity/Livre.java
+++ src/main/java/com/usmb/but3/td4biblio/entity/Livre.java
@@ -18,12 +18,8 @@ import java.time.LocalDateTime;
 @Table(name = "livre", schema = "biblio")
 public class Livre {
     @Id
-    @Column(name = "id_livre")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idLivre;
-
-    @Column(name = "gif_document", length = 200)
-    private String gifDocument;
+    @Column(name = "id_document")
+    private Integer idDocument;
 
     @Column(name = "titre_livre", nullable = false, length = 250)
     private String titreLivre;
@@ -34,51 +30,26 @@ public class Livre {
     @Column(name = "date_publication")
     private LocalDate datePublication;
 
-    @Column(name = "format_taille", length = 100)
-    private String formatTaille;
-
-    @Column(name = "description_document", length = 250)
-    private String descriptionDocument;
-
-    @Column(name = "date_acquisition")
-    private LocalDate dateAcquisition;
-
-    @Column(name = "code_emplacement", length = 10)
-    private String codeEmplacement;
-
-    @Column(name = "code_isbn", nullable = false, length = 17, unique = true)
-    private String codeIsbn;
-
-    @Column(name = "code_empruntable")
-    private Boolean codeEmpruntable;
-
-    @Column(name = "etat_document", length = 100)
-    private String etatDocument;
-
-    @Column(name = "created_at_document")
+    @Column(name = "created_at_livre")
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at_document")
+    @Column(name = "updated_at_livre")
     private LocalDateTime updatedAt;
 
     @ManyToOne
-    @JoinColumn(name = "id_document")
-    private Document idDocument;
-    
-    @ManyToOne
-    @JoinColumn(name = "id_editeur")
+    @JoinColumn(name = "id_editeur", nullable = false)
     private Editeur idEditeur;
-    
-    @ManyToOne
-    @JoinColumn(name = "id_type_document")
-    private TypeDocument idTypeDocument;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "id_document")
+    private Document document;
 
     public boolean isEqualTo(Livre livre) {
         if (this == livre) return true;
         if (livre == null) return false;
         if (idDocument != null ? !idDocument.equals(livre.idDocument) : livre.idDocument != null) return false;
         if (titreLivre != null ? !titreLivre.equals(livre.titreLivre) : livre.titreLivre != null) return false;
-        if (codeIsbn != null ? !codeIsbn.equals(livre.codeIsbn) : livre.codeIsbn != null) return false;
         if (idEditeur != null ? !idEditeur.equals(livre.idEditeur) : livre.idEditeur != null) return false;
         if (nbPages != null ? !nbPages.equals(livre.nbPages) : livre.nbPages != null) return false;
         if (datePublication != null ? !datePublication.equals(livre.datePublication) : livre.datePublication != null)
@@ -103,7 +74,6 @@ public class Livre {
         int result = 17;
         result = 31 * result + (idDocument != null ? idDocument.hashCode() : 0);
         result = 31 * result + (titreLivre != null ? titreLivre.hashCode() : 0);
-        result = 31 * result + (codeIsbn != null ? codeIsbn.hashCode() : 0);
         result = 31 * result + (idEditeur != null ? idEditeur.hashCode() : 0);
         result = 31 * result + (nbPages != null ? nbPages.hashCode() : 0);
         result = 31 * result + (datePublication != null ? datePublication.hashCode() : 0);
@@ -111,7 +81,7 @@ public class Livre {
     }
 
     public String getDesc() {
-        return titreLivre + " (ISBN: " + codeIsbn + ")";
+        return titreLivre;
     }
 
     @Override
@@ -119,7 +89,6 @@ public class Livre {
         return "Livre{" +
                 "idDocument=" + idDocument +
                 ", titreLivre='" + titreLivre + '\'' +
-                ", codeIsbn='" + codeIsbn + '\'' +
                 ", nbPages=" + nbPages +
                 ", datePublication=" + datePublication +
                 '}';
