1ff0c9c70a4c04f9c702ccf39ea0328a2f81239e
AlexisBONNARD <alexis.bonnard74@gmail.com>
2026-06-15 09:24:47 +0200

    Refactor: TypeAuteur

--- src/main/java/com/usmb/but3/td4biblio/repository/TypeAuteurRepo.java
+++ src/main/java/com/usmb/but3/td4biblio/repository/TypeAuteurRepo.java
@@ -1,5 +1,10 @@
 package com.usmb.but3.td4biblio.repository;
 
-public class TypeAuteurRepo {
-    
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.usmb.but3.td4biblio.entity.TypeAuteur;
+
+public interface TypeAuteurRepo extends JpaRepository<TypeAuteur, Integer> {
+    List<TypeAuteur> findByNom(String nom);
+    List<TypeAuteur> findByNomLike(String nom);
 }
