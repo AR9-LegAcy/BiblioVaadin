e21ed1649f02683bc6275b47f4c60d6b0dbe4833
AlexisBONNARD <alexis.bonnard74@gmail.com>
2026-06-15 11:48:25 +0200

    Add: Ecrire_Entity

---
+++ src/main/java/com/usmb/but3/td4biblio/entity/Ecrire.java
@@ -0,0 +1,74 @@
+package com.usmb.but3.td4biblio.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Une classe entité qui représente la table CLASSER de la base de données
+ */
+
+@Entity
+@Table(name = "ecrire", schema = "biblio")
+@IdClass(EcrireId.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ecrire {
+
+    @Column(name = "created_at_ecrire")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at_ecrire")
+    private LocalDateTime updatedAt;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "id_auteur")
+    private Auteur idAuteur;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "id_document")
+    private Document idDocument;
+
+    public boolean isEqualTo(Ecrire ecrire) {
+        if (ecrire == null) return false;
+
+        return idAuteur.equals(ecrire.idAuteur)
+                && idDocument.equals(ecrire.idDocument);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Ecrire)) return false;
+
+        return isEqualTo((Ecrire) obj);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+
+        result = 31 * result
+                + (idAuteur != null ? idAuteur.hashCode() : 0);
+
+        result = 31 * result
+                + (idDocument != null ? idDocument.hashCode() : 0);
+
+        return result;
+    }
+}
---
+++ src/main/java/com/usmb/but3/td4biblio/entity/EcrireId.java
@@ -0,0 +1,42 @@
+package com.usmb.but3.td4biblio.entity;
+
+import java.io.Serializable;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class EcrireId implements Serializable {
+
+    private Integer idAuteur;
+
+    private Integer idDocument;
+
+    // equals et hashCode personnalisés (optionnels si Lombok gère @Data)
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        EcrireId other = (EcrireId) obj;
+        if (idAuteur == null ? other.idAuteur != null : !idAuteur.equals(other.idAuteur))
+            return false;
+        if (idDocument == null ? other.idDocument != null : !idDocument.equals(other.idDocument))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (idAuteur == null ? 0 : idAuteur.hashCode());
+        result = prime * result + (idDocument == null ? 0 : idDocument.hashCode());
+        return result;
+    }
+}
