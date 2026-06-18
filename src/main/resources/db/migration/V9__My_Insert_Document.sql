d9bb9d6b62230f583612609b828c9c9d27862e02
k-nny <kedo42.d@gmail.com>
2026-06-18 09:45:52 +0200

    modification des dates des événements et des emprunts

--- src/main/resources/db/migration/V9__My_Insert_Document.sql
+++ src/main/resources/db/migration/V9__My_Insert_Document.sql
@@ -2,8 +2,8 @@ INSERT INTO biblio.DOCUMENT (ID_TYPE_DOCUMENT, GIF_DOCUMENT, FORMAT_TAILLE, DESC
     (1, NULL, '21x14 cm', 'Les Misérables - tome 1',            '2020-05-10', 'A-01-001', '978-2-07-040850-4', TRUE,  'Bon état',        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (1, NULL, '21x14 cm', 'L''Étranger',                        '2019-03-22', 'A-01-002', '978-2-07-036024-5', TRUE,  'Bon état',        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (2, NULL, '30x22 cm', 'Astérix le Gaulois',                 '2021-01-15', 'B-02-001', '978-2-01-210013-6', TRUE,  'Bon état',        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (1, NULL, '21x14 cm', 'Harry Potter à l''école des sorciers','2018-11-30','A-01-003', '978-2-07-054138-0', TRUE,  'Usé',             CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, NULL, '21x14 cm', 'Harry Potter à l''école des sorciers','2018-11-30','A-01-003', '978-2-07-054138-0', FALSE,  'Abimé',             CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (1, NULL, '21x14 cm', 'Germinal',                           '2022-02-08', 'A-02-001', '978-2-08-121316-7', TRUE,  'Très bon état',   CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (3, NULL, '24x17 cm', 'La Faune des Alpes',                 '2023-04-19', 'C-01-001', '978-2-03-598471-2', FALSE, 'Bon état',        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3, NULL, '24x17 cm', 'La Faune des Alpes',                 '2023-04-19', 'C-01-001', '978-2-03-598471-2', FALSE, 'Hors service',        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (1, NULL, '21x14 cm', 'Les Trois Mousquetaires',            '2020-09-05', 'A-01-004', '978-2-07-041254-9', TRUE,  'Bon état',        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (1, NULL, '21x14 cm', 'Boule de Suif',                      '2021-07-11', 'A-02-002', '978-2-07-036888-3', TRUE,  'Bon état',        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
